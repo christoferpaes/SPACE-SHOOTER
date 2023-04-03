2d6e713eb6939c218f891be940bf3d073175bca3
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2023-04-03 11:28:49 -0700

    Update GameViewController.swift

--- SpaceShooter/SpaceShooter/GameViewController.swift
+++ SpaceShooter/SpaceShooter/GameViewController.swift
@@ -2,8 +2,8 @@
 //  GameViewController.swift
 //  SpaceShooter
 //
-//  Created by Valentina Carfagno on 4/21/19.
-//  Copyright © 2019 RSC. All rights reserved.
+//  Created by Christofer Patrick Paes on 4/21/19.
+//  Copyright © 2019 Christofer Patrick Paes RSC. All rights reserved.
 //
 
 import UIKit
